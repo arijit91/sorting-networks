3e21967fa6831c92b39c877bc58a9542baf3006d
Gunaa <avgunaa@gmail.com>
2013-03-13 04:15:00 +0000

    Halver added

--- halver.py
+++ halver.py
@@ -1,10 +1,35 @@
-import sys
-import comparator
+from sys import *
+from sorter import *
+from comparator import *
+from random import *
 
-class Halver():
-  def __init__(self, numLines, epsilon):
-    self.numLines = numLines
-    self.epsilon = epsilon
+VERIFY = 1
 
-    self.stages = []
+def create_halver(numLines, epsilon):
     
+  halver = Sorter(numLines)
+  mu = 1 / epsilon
+  d = 5 #something
+  for i in range(0,d):
+    halver.add_stage(random_bipartite_stage(numLines), VERIFY)
+  return halver
+    
+def random_bipartite_stage(numLines):
+ 
+  bc = Comparator()
+  stage = []
+  setA = [i+1 for i in range(0,numLines/2)]
+  setB = [i+1 + (numLines/2) for i in range(0,numLines/2)]
+  shuffle(setA)
+  shuffle(setB)
+  print setA
+  print setB
+  for i in range(0,numLines/2):
+    stage.append((setA[i],setB[i],bc))
+  stage = tuple(stage)
+  print stage
+  return stage
+    
+halver = create_halver(20, 0.1)
+halver.display_ascii_art()
+
--- separator.py
+++ separator.py
@@ -3,3 +3,9 @@ import halver
 
 class Separator():
   def __init__(self, numLines, f, eps_b, eps_f):
+    self.numLines = numLines
+    self.f = f
+    self.eps_b = eps_b
+    self.eps_f = eps_f
+
+
